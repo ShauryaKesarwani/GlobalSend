ddaf9982ed79d2d28e86ec62acb3cddfc27956ab
GamingWithShaurya <shauryakesarwani19@gmail.com>
2026-08-03 00:17:38 +0530

    analytics

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -28,6 +29,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
+      <GoogleAnalytics gaId="G-B5PK9PMVZB" />
     </html>
   );
 }
